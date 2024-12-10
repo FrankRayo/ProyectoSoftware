8a335992ea1fc920943736ed8e2c9d2d8cfad28f
FrankRayo <163183778+FrankRayo@users.noreply.github.com>
2024-12-10 02:27:56 -0300

    Final?

--- my-app/app/i18n.js
+++ my-app/app/i18n.js
@@ -5,7 +5,7 @@ const translations = {
     login: {
       username: "Email",
       password: "Password",
-      enter: "Login",
+      enter: "Enter",
     },
     signup: {
       email: "Email",
@@ -32,17 +32,44 @@ const translations = {
     news: "NEWS",
     releaseNotes: "RELEASE NOTES",
     contactUs: "CONTACT US",
+    aboutPage: {
+      title: "About CWAEmu",
+      about1: "CWAEmu is a non-profit, fan revival of SOE's Clone Wars Adventures Online. Players will dive into a 100% free-to-play, action-packed virtual world where they can live out the thrills and excitement of Star Wars: The Clone Wars first-hand!",
+      about2: "Jam-packed with activities, players will battle it out alongside the familiar faces of Anakin Skywalker, Obi-Wan Kenobi, Yoda, and Ahsoka Tano. Expect fun minigames, daily activities, events, rewards, lively social environments, an in-depth housing system, and friendly competition.",
+      about3: "As CWAEMU is an online experience, we are determined to provide a safe and rewarding environment with your children in mind.",
+    },
+    closedTestingPage: {
+      title: "CLOSED TESTING REGISTRATION NOW OPEN!",
+      greeting: "Hello There,",
+      introduction: "Before we get started the team would like to wish everyone a very happy new year!",
+      overview: "With the new year, we have decided to take a new direction for the game so we have introduced a few changes to alpha testing and our roadmap. Let's jump into it!",
+      alphaTesting: {
+        title: "ALPHA TESTING ACCESS",
+        description: "As of this year, accessing the closed alpha will be restricted to application only. The sign up information is available on the homepage of the cwaemu website! Now you might be wondering, why did we choose to go with this method? Here is why:",
+        points: [
+          "First of all, going by this method, people who aren't on the discord can get access to the builds!",
+          "Secondly, with people applying, we can now bring in more testers at a time, without having to host an event, so that we can stress test the servers.",
+          "Thirdly, it gives us the ability to test the game for individual platforms!",
+        ],
+      },
+      roadmapChanges: {
+        title: "ROADMAP CHANGES",
+        description: "We have decided to expand the roadmap, moving up housing to be our next big objective, following social abilities and quality of life.",
+        details: "We've also decided to let the community choose what minigames we should focus on, the roadmap showcasing two different minigames milestones, which once hovered over would reveal three spots, marked as 'TBA: COMMUNITY VOTE', so stay tuned for that, coming soon™!",
+      },
+      closing: "Thank you for following the development of Clone Wars Adventures Emulator, we wish you a very happy 2021, and we are looking forward to seeing you in-game!",
+    },
   },
   es: {
     welcome: "Bienvenido al sitio de prueba de CWAEmu",
     description: "Este es un sitio web de muestra construido con Next.js y Tailwind CSS para CWAEmu.",
     login: {
-      username: "Correo electrónico",
+      username: "Correo Electrónico",
       password: "Contraseña",
       enter: "Entrar",
     },
     signup: {
-      email: "Correo electrónico",
+      email: "Correo Electrónico",
       password: "Contraseña",
       signUpButton: "Registrar",
       invalidEmail: "Formato de correo electrónico no válido",
@@ -66,6 +93,33 @@ const translations = {
     news: "NOTICIAS",
     releaseNotes: "NOTAS DE PARCHE",
     contactUs: "CONTÁCTANOS",
+    aboutPage: {
+      title: "Acerca de CWAEmu",
+      about1: "CWAEmu es un proyecto sin fines de lucro que revive el juego Clone Wars Adventures Online de SOE. Los jugadores podrán sumergirse en un mundo virtual lleno de acción, 100% gratuito, donde podrán experimentar de primera mano las emociones y la emoción de Star Wars: The Clone Wars.",
+      about2: "Repleto de actividades, los jugadores lucharán junto a rostros familiares como Anakin Skywalker, Obi-Wan Kenobi, Yoda y Ahsoka Tano. Podrán disfrutar de divertidos minijuegos, actividades diarias, eventos, recompensas, entornos sociales vibrantes, un sistema de vivienda detallado y competencia amistosa.",
+      about3: "Dado que CWAEMU es una experiencia en línea, estamos comprometidos a proporcionar un entorno seguro y gratificante, pensado especialmente para sus hijos.",
+    },
+    closedTestingPage: {
+      title: "¡EL REGISTRO PARA PRUEBAS CERRADAS ESTÁ ABIERTO!",
+      greeting: "Hola a todos,",
+      introduction: "Antes de comenzar, el equipo desea desearles a todos un muy feliz año nuevo.",
+      overview: "Con el nuevo año, hemos decidido tomar una nueva dirección para el juego, por lo que hemos introducido algunos cambios en las pruebas alfa y nuestra hoja de ruta. ¡Entremos en ello!",
+      alphaTesting: {
+        title: "ACCESO A LA PRUEBA ALFA",
+        description: "A partir de este año, el acceso a la alfa cerrada estará restringido solo a solicitud. ¡La información de registro está disponible en la página de inicio del sitio web de cwaemu! Ahora podrías preguntarte, ¿por qué elegimos este método? Aquí está la razón:",
+        points: [
+          "En primer lugar, siguiendo este método, ¡las personas que no están en el Discord pueden acceder a las versiones!",
+          "En segundo lugar, con las solicitudes, ahora podemos traer más testers a la vez, sin tener que organizar un evento, para que podamos realizar pruebas de esfuerzo en los servidores.",
+          "En tercer lugar, ¡nos da la capacidad de probar el juego en plataformas individuales!",
+        ],
+      },
+      roadmapChanges: {
+        title: "CAMBIOS EN LA HOJA DE RUTA",
+        description: "Hemos decidido ampliar la hoja de ruta, adelantando la vivienda como nuestro próximo gran objetivo, seguido de habilidades sociales y calidad de vida.",
+        details: "También hemos decidido dejar que la comunidad elija qué minijuegos debemos priorizar. La hoja de ruta muestra dos hitos de minijuegos diferentes, que, al pasar el cursor, revelarían tres espacios marcados como 'TBA: VOTO COMUNITARIO'. ¡Así que estén atentos a eso, próximamente™!",
+      },
+      closing: "Gracias por seguir el desarrollo de Clone Wars Adventures Emulator, les deseamos un muy feliz 2021 y esperamos verlos en el juego.",
+    },
   },
 };
 
